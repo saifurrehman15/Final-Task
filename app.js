d19944baad606955b7a947362928da1347c98e5b
saifurrehman15 <saifkhandada72@gmail.com>
2024-08-10 11:33:50 +0500

    updated

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-let input = ["bat", "tap", "cat", "tab", "pat"];
-let ana = [];
+let input = ["heart", "earth", "cat", "tab", "pat"];
+let anagoram= [];
 
 for (let i = 0; i < input.length; i++) {
-  let sortedWord = input[i].split("").sort().join("");
+  let save = input[i].split("").sort().join("");
 
-  let found = false;
-  for (let j = 0; j < ana.length; j++) {
-    if (ana[j][0].split("").sort().join("") === sortedWord) {
-      ana[j].push(input[i]);
-      found = true;
+  let flag = false;
+  for (let j = 0; j < anagoram.length; j++) {
+    if (anagoram[j][0].split("").sort().join("") === save) {
+      anagoram[j].push(input[i]);
+      flag = true;
       break;
     }
   }
 
-  if (!found) {
-    ana.push([input[i]]);
+  if (!flag) {
+    anagoram.push([input[i]]);
   }
 }
 
-console.log(ana);
+console.log(anagoram);
